ce5db79e9bac92a308040a1f32a72a760caa5097
EMajesty <lassi.laitinen@protonmail.com>
2025-03-27 19:19:42 +0200

    stuff

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 https://github.com/esp-rs/esp-idf-template
 https://github.com/david-sawatzke/hub75-rs
 https://github.com/hzeller/rpi-rgb-led-matrix/blob/master/wiring.md
+
+https://blog.davidv.dev/posts/exploring-hub75/
+https://github.com/DavidVentura/hub75-esp
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use anyhow::{self};
-use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+// use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
     hal::{delay::FreeRtos, gpio::PinDriver, prelude::Peripherals},
-    nvs::EspDefaultNvsPartition,
-    wifi::EspWifi,
+    // nvs::EspDefaultNvsPartition,
+    // wifi::EspWifi,
 };
-use hub75::Hub75;
 
-mod secrets;
-use secrets::{WIFI_PASSWORD, WIFI_SSID};
+mod hub75;
 
-fn main() -> anyhow::Result<()> {
+// mod secrets;
+// use secrets::{WIFI_PASSWORD, WIFI_SSID};
+
+fn main() -> anyhow::Result<()>{
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -20,58 +21,72 @@ fn main() -> anyhow::Result<()> {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take()?;
-    let sysloop = EspSystemEventLoop::take()?;
-    let nvs = EspDefaultNvsPartition::take()?;
-    let mut delay = FreeRtos;
+    // let sysloop = EspSystemEventLoop::take()?;
+    // let nvs = EspDefaultNvsPartition::take()?;
+    // let mut delay = FreeRtos;
 
-    let r1 = PinDriver::output(peripherals.pins.gpio25).unwrap();
-    let g1 = PinDriver::output(peripherals.pins.gpio26).unwrap();
-    let b1 = PinDriver::output(peripherals.pins.gpio27).unwrap();
-    let r2 = PinDriver::output(peripherals.pins.gpio14).unwrap();
-    let g2 = PinDriver::output(peripherals.pins.gpio12).unwrap();
-    let b2 = PinDriver::output(peripherals.pins.gpio13).unwrap();
-    let a = PinDriver::output(peripherals.pins.gpio23).unwrap();
-    let b = PinDriver::output(peripherals.pins.gpio19).unwrap();
-    let c = PinDriver::output(peripherals.pins.gpio5).unwrap();
-    let d = PinDriver::output(peripherals.pins.gpio17).unwrap();
-    let e = PinDriver::output(peripherals.pins.gpio32).unwrap();
-    let clk = PinDriver::output(peripherals.pins.gpio16).unwrap();
-    let lat = PinDriver::output(peripherals.pins.gpio4).unwrap();
-    let oe = PinDriver::output(peripherals.pins.gpio15).unwrap();
+    let mut r1 = PinDriver::output(peripherals.pins.gpio25)?;
+    let g1 = PinDriver::output(peripherals.pins.gpio26)?;
+    let b1 = PinDriver::output(peripherals.pins.gpio27)?;
+    let r2 = PinDriver::output(peripherals.pins.gpio14)?;
+    let g2 = PinDriver::output(peripherals.pins.gpio12)?;
+    let b2 = PinDriver::output(peripherals.pins.gpio13)?;
+    let a = PinDriver::output(peripherals.pins.gpio23)?;
+    let b = PinDriver::output(peripherals.pins.gpio19)?;
+    let c = PinDriver::output(peripherals.pins.gpio5)?;
+    let d = PinDriver::output(peripherals.pins.gpio17)?;
+    let e = PinDriver::output(peripherals.pins.gpio32)?;
+    let mut clk = PinDriver::output(peripherals.pins.gpio16)?;
+    let lat = PinDriver::output(peripherals.pins.gpio4)?;
+    let oe = PinDriver::output(peripherals.pins.gpio15)?;
 
-    let display_pins = (r1, g1, b1, r2, g2, b2, a, b, c, d, e, clk, lat, oe);
-
-    let mut display = Hub75::new(display_pins, 4);
-
-    let mut wifi = EspWifi::new(peripherals.modem, sysloop, Some(nvs))?;
-
-    let config = Configuration::Client(ClientConfiguration {
-        ssid: WIFI_SSID.try_into().unwrap(),
-        password: WIFI_PASSWORD.try_into().unwrap(),
-        auth_method: AuthMethod::WPA2Personal,
-        ..Default::default()
-    });
-
-    wifi.set_configuration(&config)?;
-    wifi.start()?;
-    wifi.connect()?;
-
-    while !wifi.is_connected()? {
-        log::info!("Waiting for connection");
-    }
-
-    log::info!("Connected");
-
-    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
+    // let mut wifi = EspWifi::new(peripherals.modem, sysloop, Some(nvs))?;
+    //
+    // let config = Configuration::Client(ClientConfiguration {
+    //     ssid: WIFI_SSID.try_into()?,
+    //     password: WIFI_PASSWORD.try_into()?,
+    //     auth_method: AuthMethod::WPA2Personal,
+    //     ..Default::default()
+    // });
+    //
+    // wifi.set_configuration(&config)?;
+    // wifi.start()?;
+    // wifi.connect()?;
+    //
+    // while !wifi.is_connected()? {
+    //     log::info!("Waiting for connection");
+    // }
+    //
+    // log::info!("Connected");
+    
+    r1.set_high()?;
+    clk.set_high()?;
+    clk.set_low()?;
 
     loop {
-        // display.output(&mut delay)?;
-        led.set_high()?;
-        FreeRtos::delay_ms(500);
-        led.set_low()?;
-        FreeRtos::delay_ms(500);
     }
 }
 
-fn clock_tick() {
-}
+// r1 2
+// g1 4
+// b1 5
+// r2 18
+// g2 19
+// b2 21
+// a 12
+// b 13
+// c 14
+// d 15
+// e 16
+// clk 25
+// lat 26
+// oe 27
+//
+// r1 | g1
+// b1 | gnd
+// r2 | g2
+// b2 | e
+// a  | b
+// c  | d
+// clk| lat
+// oe | gnd
